4d4a66a961b588bf9becb5ea226106b03c815386
David-Kennedy-Docusign <David.Kennedy@docusign.com>
2021-02-02 01:31:16 -0800

    Delete officeRouter.js
    Remove dated officeRouter

--- routes/officeRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const officeController = require('../controllers/officeController');
-
-router.get('/', officeController.get_office);
-router.post('/', officeController.add_office_to_session);
-
-module.exports = router;
