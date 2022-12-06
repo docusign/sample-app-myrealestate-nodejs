c95ae4e79bbf4aaf3db865a0e5e029266006925c
Inbar Gazit <inbar.gazit@docusign.com>
2022-12-06 10:44:12 -0800

    Fixing bad links

--- client/src/components/UI/Info/InfoContent/LoginInfo/LoginInfo.js
+++ client/src/components/UI/Info/InfoContent/LoginInfo/LoginInfo.js
@@ -11,7 +11,7 @@ const loginInfo = (props) => (
         <ul>
             <li>{textContent.login.info.listItem1}</li>
             <li>{textContent.login.info.listItem2}</li>
-            <li>{textContent.login.info.listItem3Part1} <a href='https://developers.docusign.com/rooms-api/reference/Offices' target='_blank' rel="noopener noreferrer">{textContent.login.info.listItem3Part2}</a></li>
+            <li>{textContent.login.info.listItem3Part1} <a href='https://developers.docusign.com/docs/rooms-api/reference/Offices' target='_blank' rel="noopener noreferrer">{textContent.login.info.listItem3Part2}</a></li>
         </ul>
 
         <div className={classes.CodeFlow}>
@@ -19,9 +19,9 @@ const loginInfo = (props) => (
             <h3>{textContent.login.info.step1}</h3>
             <p><a href="https://developers.docusign.com/rooms-api/guides/auth" target="_blank" rel="noopener noreferrer">{textContent.login.info.step1info1}</a> {textContent.login.info.step1info2}</p>
             <h3>{textContent.login.info.step2}</h3>
-            <p>{textContent.login.info.step2info1} <a href="https://developers.docusign.com/rooms-api/reference/Users/Users/GetUsers" target="_blank" rel="noopener noreferrer">{textContent.login.info.step2info2}</a></p>
+            <p>{textContent.login.info.step2info1} <a href="https://developers.docusign.com/docs/rooms-api/reference/Users/Users/GetUsers" target="_blank" rel="noopener noreferrer">{textContent.login.info.step2info2}</a></p>
             <h3>{textContent.login.info.step3}</h3>
-            <p>{textContent.login.info.step3info1} <a href='https://developers.docusign.com/rooms-api/reference/Offices/Offices/CreateOffice' target='_blank' rel="noopener noreferrer">{textContent.login.info.step3info2}</a> {textContent.login.info.step3info3} <a href='https://developers.docusign.com/rooms-api/reference/Users/Users/AddUserToOffice' target='_blank' rel="noopener noreferrer">{textContent.login.info.step3info4}</a>{textContent.login.info.step3info5}</p>
+            <p>{textContent.login.info.step3info1} <a href='https://developers.docusign.com/docs/rooms-api/reference/Offices/Offices/CreateOffice' target='_blank' rel="noopener noreferrer">{textContent.login.info.step3info2}</a> {textContent.login.info.step3info3} <a href='https://developers.docusign.com/docs/rooms-api/reference/Users/Users/AddUserToOffice' target='_blank' rel="noopener noreferrer">{textContent.login.info.step3info4}</a>{textContent.login.info.step3info5}</p>
         </div>
     </div>
 );
